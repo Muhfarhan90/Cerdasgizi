406cee3e0db28ca9d1c8c2fd5fcae077b920573a
Muhfarhan90 <muhfarhanhidayatulloh@gmail.com>
2024-06-06 15:04:46 +0700

    update search ahligizi

--- page/admin/ahligizi.php
+++ page/admin/ahligizi.php
@@ -2,12 +2,12 @@
 include("../../logout.php");
 include("../../database/database.php");
 
-if (isset($_POST['cari'])) {
-  $katakunci = $_POST['katakunci'];
+// if (isset($_POST['cari'])) {
+//   $katakunci = $_POST['katakunci'];
 
-  $query = "SELECT * FROM nutritionist WHERE `FULLNAME_NUTRITIONIST` = '$katakunci'";
-  $result = mysqli_query($conn, $query);
-}
+//   $query = "SELECT * FROM nutritionist WHERE `FULLNAME_NUTRITIONIST` = '$katakunci'";
+//   $result = mysqli_query($conn, $query);
+// }
 ?>
 
 <!DOCTYPE html>
@@ -105,155 +105,6 @@ if (isset($_POST['cari'])) {
           </div>
         </div>
       </div>
-      <div id="right-sidebar" class="settings-panel">
-        <i class="settings-close typcn typcn-times"></i>
-        <ul class="nav nav-tabs" id="setting-panel" role="tablist">
-          <li class="nav-item">
-            <a class="nav-link active" id="todo-tab" data-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" id="chats-tab" data-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
-          </li>
-        </ul>
-        <div class="tab-content" id="setting-content">
-          <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
-            <div class="add-items d-flex px-3 mb-0">
-              <form class="form w-100">
-                <div class="form-group d-flex">
-                  <input type="text" class="form-control todo-list-input" placeholder="Add To-do">
-                  <button type="submit" class="add btn btn-primary todo-list-add-btn" id="add-task">Add</button>
-                </div>
-              </form>
-            </div>
-            <div class="list-wrapper px-3">
-              <ul class="d-flex flex-column-reverse todo-list">
-                <li>
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox">
-                      Team review meeting at 3.00 PM
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li>
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox">
-                      Prepare for presentation
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li>
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox">
-                      Resolve all the low priority tickets due today
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li class="completed">
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox" checked>
-                      Schedule meeting for next week
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li class="completed">
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox" checked>
-                      Project review
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-              </ul>
-            </div>
-            <div class="events py-4 border-bottom px-3">
-              <div class="wrapper d-flex mb-2">
-                <i class="typcn typcn-media-record-outline text-primary mr-2"></i>
-                <span>Feb 11 2018</span>
-              </div>
-              <p class="mb-0 font-weight-thin text-gray">Creating component page</p>
-              <p class="text-gray mb-0">build a js based app</p>
-            </div>
-            <div class="events pt-4 px-3">
-              <div class="wrapper d-flex mb-2">
-                <i class="typcn typcn-media-record-outline text-primary mr-2"></i>
-                <span>Feb 7 2018</span>
-              </div>
-              <p class="mb-0 font-weight-thin text-gray">Meeting with Alisa</p>
-              <p class="text-gray mb-0 ">Call Sarah Graves</p>
-            </div>
-          </div>
-          <!-- To do section tab ends -->
-          <div class="tab-pane fade" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
-            <div class="d-flex align-items-center justify-content-between border-bottom">
-              <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
-              <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 font-weight-normal">See All</small>
-            </div>
-            <ul class="chat-list">
-              <li class="list active">
-                <div class="profile"><img src="images/faces/face1.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Thomas Douglas</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">19 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face2.jpg" alt="image"><span class="offline"></span></div>
-                <div class="info">
-                  <div class="wrapper d-flex">
-                    <p>Catherine</p>
-                  </div>
-                  <p>Away</p>
-                </div>
-                <div class="badge badge-success badge-pill my-auto mx-2">4</div>
-                <small class="text-muted my-auto">23 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face3.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Daniel Russell</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">14 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face4.jpg" alt="image"><span class="offline"></span></div>
-                <div class="info">
-                  <p>James Richardson</p>
-                  <p>Away</p>
-                </div>
-                <small class="text-muted my-auto">2 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face5.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Madeline Kennedy</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">5 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face6.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Sarah Graves</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">47 min</small>
-              </li>
-            </ul>
-          </div>
-          <!-- chat tab ends -->
-        </div>
-      </div>
       <!-- partial -->
       <!-- partial:partials/_sidebar.html -->
       <?php
@@ -305,8 +156,41 @@ if (isset($_POST['cari'])) {
                         $result = mysqli_query($conn, $query);
                         $i = 1;
                         while ($row = mysqli_fetch_assoc($result)) {
-                          $mime_type = "image/jpg";
                       ?>
+                          <tr>
+                            <td><?= $i ?></td>
+                            <td><?= $row['FULLNAME_NUTRITIONIST'] ?></td>
+                            <td><?= $row['EMAIL_NUTRITIONIST'] ?></td>
+                            <td><?= $row['YEARS_OF_EXPERIENCE'] ?></td>
+                            <td><?= $row['EDUCATION'] ?></td>
+                            <td><?= $row['CERTIFICATION'] ?></td>
+                            <td>
+                              <img src="../../images/icons8-doctor-32.png" alt="profil">
+                            </td>
+                            <td>
+                              <div class="d-flex align-items-center">
+                                <a href="edit-ahligizi.php?id=<?= $row['ID_NUTRITIONIST'] ?>"> <button type="button" class="btn btn-success btn-sm btn-icon-text mr-3">
+                                    Edit
+                                    <i class="typcn typcn-edit btn-icon-append"></i>
+                                  </button></a>
+                                <a href="hapus-ahligizi.php?id=<?= $row['ID_NUTRITIONIST'] ?>&id_user=<?= $row['ID_USER'] ?>" onclick="return confirm('Apakah anda yakin menghapus data ahli gizi?')"> <button type="button" class="btn btn-danger btn-sm btn-icon-text">
+                                    Delete
+                                    <i class="typcn typcn-delete-outline btn-icon-append"></i>
+                                  </button></a>
+
+                              </div>
+                            </td>
+                          </tr>
+                        <?php
+                          $i++;
+                        }
+                      } else {
+                        $keyword = $_POST['katakunci'];
+                        $query = "SELECT * FROM nutritionist WHERE `FULLNAME_NUTRITIONIST` LIKE '%$keyword%'";
+                        $result = mysqli_query($conn, $query);
+                        $i = 1;
+                        while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
                           <tr>
                             <td><?= $i ?></td>
                             <td><?= $row['FULLNAME_NUTRITIONIST'] ?></td>
--- page/admin/dashboard-admin.php
+++ page/admin/dashboard-admin.php
@@ -18,7 +18,7 @@ include('../../database/database.php');
   <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
   <!-- endinject -->
   <link rel="shortcut icon" href="../../images/favicon.png" />
-  
+
 </head>
 
 <body>
@@ -53,155 +53,7 @@ include('../../database/database.php');
           </div>
         </div>
       </div>
-      <div id="right-sidebar" class="settings-panel">
-        <i class="settings-close typcn typcn-times"></i>
-        <ul class="nav nav-tabs" id="setting-panel" role="tablist">
-          <li class="nav-item">
-            <a class="nav-link active" id="todo-tab" data-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" id="chats-tab" data-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
-          </li>
-        </ul>
-        <div class="tab-content" id="setting-content">
-          <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
-            <div class="add-items d-flex px-3 mb-0">
-              <form class="form w-100">
-                <div class="form-group d-flex">
-                  <input type="text" class="form-control todo-list-input" placeholder="Add To-do">
-                  <button type="submit" class="add btn btn-primary todo-list-add-btn" id="add-task">Add</button>
-                </div>
-              </form>
-            </div>
-            <div class="list-wrapper px-3">
-              <ul class="d-flex flex-column-reverse todo-list">
-                <li>
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox">
-                      Team review meeting at 3.00 PM
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li>
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox">
-                      Prepare for presentation
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li>
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox">
-                      Resolve all the low priority tickets due today
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li class="completed">
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox" checked>
-                      Schedule meeting for next week
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-                <li class="completed">
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input class="checkbox" type="checkbox" checked>
-                      Project review
-                    </label>
-                  </div>
-                  <i class="remove typcn typcn-delete-outline"></i>
-                </li>
-              </ul>
-            </div>
-            <div class="events py-4 border-bottom px-3">
-              <div class="wrapper d-flex mb-2">
-                <i class="typcn typcn-media-record-outline text-primary mr-2"></i>
-                <span>Feb 11 2018</span>
-              </div>
-              <p class="mb-0 font-weight-thin text-gray">Creating component page</p>
-              <p class="text-gray mb-0">build a js based app</p>
-            </div>
-            <div class="events pt-4 px-3">
-              <div class="wrapper d-flex mb-2">
-                <i class="typcn typcn-media-record-outline text-primary mr-2"></i>
-                <span>Feb 7 2018</span>
-              </div>
-              <p class="mb-0 font-weight-thin text-gray">Meeting with Alisa</p>
-              <p class="text-gray mb-0 ">Call Sarah Graves</p>
-            </div>
-          </div>
-          <!-- To do section tab ends -->
-          <div class="tab-pane fade" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
-            <div class="d-flex align-items-center justify-content-between border-bottom">
-              <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
-              <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 font-weight-normal">See All</small>
-            </div>
-            <ul class="chat-list">
-              <li class="list active">
-                <div class="profile"><img src="images/faces/face1.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Thomas Douglas</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">19 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face2.jpg" alt="image"><span class="offline"></span></div>
-                <div class="info">
-                  <div class="wrapper d-flex">
-                    <p>Catherine</p>
-                  </div>
-                  <p>Away</p>
-                </div>
-                <div class="badge badge-success badge-pill my-auto mx-2">4</div>
-                <small class="text-muted my-auto">23 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face3.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Daniel Russell</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">14 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face4.jpg" alt="image"><span class="offline"></span></div>
-                <div class="info">
-                  <p>James Richardson</p>
-                  <p>Away</p>
-                </div>
-                <small class="text-muted my-auto">2 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face5.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Madeline Kennedy</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">5 min</small>
-              </li>
-              <li class="list">
-                <div class="profile"><img src="images/faces/face6.jpg" alt="image"><span class="online"></span></div>
-                <div class="info">
-                  <p>Sarah Graves</p>
-                  <p>Available</p>
-                </div>
-                <small class="text-muted my-auto">47 min</small>
-              </li>
-            </ul>
-          </div>
-          <!-- chat tab ends -->
-        </div>
-      </div>
+
       <!-- partial -->
       <!-- partial:partials/_sidebar.html -->
       <?php
@@ -217,8 +69,9 @@ include('../../database/database.php');
             </div>
           </div>
           <div class="row">
-            <div class="col-md-4 grid-margin stretch-card">
-              <div class="card">
+            <!-- pasien -->
+            <div class="col-md-3 grid-margin stretch-card">
+              <div class="card bg-success">
                 <div class="card-body">
                   <div class="chartjs-size-monitor">
                     <div class="chartjs-size-monitor-expand">
@@ -245,14 +98,15 @@ include('../../database/database.php');
                         ?>
                       </h1>
                     </div>
-                    <i class="typcn typcn-briefcase icon-xl text-secondary"></i>
+                    <i class="typcn typcn-briefcase icon-xl text-light"></i>
                   </div>
                   <canvas id="expense-chart" height="186" width="698" style="display: block; height: 149px; width: 559px;" class="chartjs-render-monitor"></canvas>
                 </div>
               </div>
             </div>
-            <div class="col-md-4 grid-margin stretch-card">
-              <div class="card">
+            <!-- ahligizi -->
+            <div class="col-md-3 grid-margin stretch-card">
+              <div class="card bg-secondary">
                 <div class="card-body">
                   <div class="chartjs-size-monitor">
                     <div class="chartjs-size-monitor-expand">
@@ -279,14 +133,98 @@ include('../../database/database.php');
                         ?>
                       </h1>
                     </div>
-                    <i class="typcn typcn-briefcase icon-xl text-secondary"></i>
+                    <i class="typcn typcn-briefcase icon-xl text-light"></i>
+                  </div>
+                  <canvas id="expense-chart" height="186" width="698" style="display: block; height: 149px; width: 559px;" class="chartjs-render-monitor"></canvas>
+                </div>
+              </div>
+            </div>
+            <!-- konsultasi -->
+            <div class="col-md-3 grid-margin stretch-card">
+              <div class="card bg-warning">
+                <div class="card-body">
+                  <div class="chartjs-size-monitor">
+                    <div class="chartjs-size-monitor-expand">
+                      <div class=""></div>
+                    </div>
+                    <div class="chartjs-size-monitor-shrink">
+                      <div class=""></div>
+                    </div>
+                  </div>
+                  <div class="d-flex align-items-center justify-content-between justify-content-md-center justify-content-xl-between flex-wrap mb-4">
+                    <div>
+                      <p class="mb-2 text-md-center text-lg-left">Total Konsultasi</p>
+                      <h1 class="mb-0">
+                        <?php
+                        $query = "SELECT COUNT(*) as total FROM consultation";
+                        $result = mysqli_query($conn, $query);
+                        if (mysqli_num_rows($result) > 0) {
+                          $row = mysqli_fetch_assoc($result);
+                          $total_rows = $row['total'];
+                          echo "$total_rows";
+                        } else {
+                          echo "0";
+                        }
+                        ?>
+                      </h1>
+                    </div>
+                    <i class="typcn typcn-briefcase icon-xl text-light"></i>
+                  </div>
+                  <canvas id="expense-chart" height="186" width="698" style="display: block; height: 149px; width: 559px;" class="chartjs-render-monitor"></canvas>
+                </div>
+              </div>
+            </div>
+            <!-- artikel -->
+            <div class="col-md-3 grid-margin stretch-card">
+              <div class="card bg-info">
+                <div class="card-body">
+                  <div class="chartjs-size-monitor">
+                    <div class="chartjs-size-monitor-expand">
+                      <div class=""></div>
+                    </div>
+                    <div class="chartjs-size-monitor-shrink">
+                      <div class=""></div>
+                    </div>
+                  </div>
+                  <div class="d-flex align-items-center justify-content-between justify-content-md-center justify-content-xl-between flex-wrap mb-4">
+                    <div>
+                      <p class="mb-2 text-md-center text-lg-left">Total Artikel</p>
+                      <h1 class="mb-0">
+                        <?php
+                        $query = "SELECT COUNT(*) as total FROM article";
+                        $result = mysqli_query($conn, $query);
+                        if (mysqli_num_rows($result) > 0) {
+                          $row = mysqli_fetch_assoc($result);
+                          $total_rows = $row['total'];
+                          echo "$total_rows";
+                        } else {
+                          echo "0";
+                        }
+                        ?>
+                      </h1>
+                    </div>
+                    <i class="typcn typcn-briefcase icon-xl text-light"></i>
                   </div>
                   <canvas id="expense-chart" height="186" width="698" style="display: block; height: 149px; width: 559px;" class="chartjs-render-monitor"></canvas>
                 </div>
               </div>
             </div>
           </div>
-          
+          <div class="row">
+            <div class="card">
+              <!-- <div class="card-title">
+                <h4>Gender Pasien</h4>
+              </div> -->
+              <div class="card-body">
+                <div>
+                  <canvas id="myChart"></canvas>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+
 
           <!-- partial -->
         </div>
@@ -312,6 +250,34 @@ include('../../database/database.php');
   <!-- Custom js for this page-->
   <script src="../../js/dashboard.js"></script>
   <!-- End custom js for this page-->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script>
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    // const config = {
+    //   type: 'doughnut',
+    //   data: data,
+    // };
+  </script>
 </body>
 
 </html>
