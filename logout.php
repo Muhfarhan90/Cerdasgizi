a565af4bc81181f0e87633edaa0c97b88591a6c5
mazayaaa <fatharanimazayaap@gmail.com>
2024-06-18 17:15:02 +0700

    remove vscode settings json

--- logout.php
+++ logout.php
@@ -4,9 +4,10 @@ if (isset($_POST["logout"])) {
   $_SESSION['is_login'] = false;
   session_unset();
   session_destroy();
-  echo "<script>
-  window.location.href = '../../index.php';
-  </script>";
+  // echo "<script>
+  // window.location.href = '../../index.php';
+  // </script>";
+  header("Location: http://localhost/Website-PHP-Cerdasgizi");
   // header("Location: http://localhost/cerdasgizi.com/index.php");
   exit();
 }
